f07767f55c726b52411d8225de12cc69e597d6a4
ByteOtter <christopher-hock@protonmail.com>
2024-09-27 14:14:18 +0200

    scrub array index from query strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 // Your library code goes here.
 #[macro_use]
-
 extern crate serde;
 extern crate reqwest;
 
-pub mod util;
 pub mod paths;
 pub mod types;
+pub mod util;
 pub mod version;
--- src/version.rs
+++ src/version.rs
@@ -1,2 +1,2 @@
 #[allow(dead_code)]
-pub const VERSION: &str = "2.1.0";
+pub const VERSION: &str = "2.2.0";
